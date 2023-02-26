97b2b905a48daf8dd0cb808454f22eaa925631e1
etrl-z <emiliotorella@gmail.com>
2023-02-26 15:22:57 +0100

    database sync

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,25 +1,25 @@
 import React from 'react'
+import { useState, useRef } from 'react';
 
-function Card({ data, url }) {
+function Card({ title, url }) {
 
-    const audio = new Audio(url)
-
+    const [audio] = useState(typeof Audio !== "undefined" && new Audio(url));
     const play = function () {
         audio.play();
     }
-
     const pause = function () {
         audio.pause();
     }
-
     const stop = function () {
-        location.reload();
+        audio.pause();
+        audio.currentTime = 0;
     }
 
     return (
         <div class="card">
-            <p>{data.title}</p>
+            <p>{title}</p>
             <div>
+                <input type="range" min="1" max="100" class="slider" />
                 <button onClick={play}>PLAY</button>
                 <button onClick={pause}>PAUSE</button>
                 <button onClick={stop}>STOP</button>
@@ -30,4 +30,3 @@ function Card({ data, url }) {
 }
 
 export default Card
-
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,23 +1,10 @@
 import Head from 'next/head'
 import styles from '@/styles/Home.module.css'
-import Card from '@/components/Card'
-import { db } from '../firebaseConfig';
-import { collection, query, orderBy } from "firebase/firestore";
-import { useCollection } from "react-firebase-hooks/firestore";
+import { db, storage } from "../firebaseConfig"
+import { getStorage, ref, getDownloadURL, list } from "firebase/storage";
+import Card from '@/components/Card';
 
-export default function Home() {
-
-  const testURLS = [];
-  for (let index = 0; index < 10; index++) {
-    var url = `https://www.soundhelix.com/examples/mp3/SoundHelix-Song-${index}.mp3`;
-    testURLS.push(url);
-  }
-
-  const collectionRef = collection(db, "files");
-
-  const [filesCollection] = useCollection(
-    query(collectionRef, orderBy("title", "asc"))
-  );
+export default function Home(props) {
 
   return (
     <>
@@ -27,12 +14,36 @@ export default function Home() {
       </Head>
 
       <main className={styles.main}>
-        {filesCollection?.docs.map((file, i) => {
+        {props.data.map((file) => {
           return (
-            <Card data={file.data()} url={testURLS[i]} />
+            <Card title={file.title} url={file.url} />
           )
         })}
       </main>
     </>
   )
+}
+
+export async function getServerSideProps(context) {
+
+  const storage = getStorage();
+  const listRef = ref(storage, 'audio');
+
+  const result = await list(listRef, { maxResults: 100 });
+
+  const data = [];
+  for (let i = 0; i < result.items.length; i++) {
+    var res = await getDownloadURL(ref(storage, `audio/${result.items[i].name}`))
+    var _url = res
+    data[i] = {
+      title: result.items[i].name.replace(".opus", ""),
+      url: _url
+    }
+  }
+
+  return {
+    props: {
+      data
+    }
+  };
 }
