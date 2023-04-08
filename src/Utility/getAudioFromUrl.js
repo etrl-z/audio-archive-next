321965581115a2725858bf849bd303bb083fcb03
etrl-z <emiliotorella@gmail.com>
2023-04-08 20:01:52 +0200

    getAudioFromUrl

--- src/Utility/getAudioFromUrl.js
+++ src/Utility/getAudioFromUrl.js
@@ -2,7 +2,6 @@ import React from 'react'
 import { useState } from 'react';
 
 export default function getAudioFromUrl(url) {
-
     const [audio] = useState(typeof Audio !== "undefined" && new Audio(url));
     return audio;
 }
